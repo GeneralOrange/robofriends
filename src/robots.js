e6c2eab2ab636139e98c872d82b0998957971da6
Erik van der Schaaf <erik.17021995@gmail.com>
2018-09-25 15:28:06 +0200

    Building a Robot App

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 2,
+		name: 'Erik van der Schaaf',
+		username: 'General Orange',
+		email: 'erik@rubixmarketing.nl'
+	},
+	{
+		id: 3,
+		name: 'Nicolette van der Marel',
+		username: 'Blond Ding',
+		email: 'Nicolette@rubixmarketing.nl'
+	},
+	{
+		id: 4,
+		name: 'Esther',
+		username: 'Esther',
+		email: 'Esther@rubixmarketing.nl'
+	}
+]
